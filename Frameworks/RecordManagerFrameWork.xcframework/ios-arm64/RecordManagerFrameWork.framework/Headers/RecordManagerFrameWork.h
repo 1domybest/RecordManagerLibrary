276a6cc1d9d89b883de427312f146c6994c98363
OTIS <72382663+1domybest@users.noreply.github.com>
2024-11-05 14:45:18 +0900

    add
    add

---
+++ Frameworks/RecordManagerFrameWork.xcframework/ios-arm64/RecordManagerFrameWork.framework/Headers/RecordManagerFrameWork.h
@@ -0,0 +1,18 @@
+//
+//  RecordManagerFrameWork.h
+//  RecordManagerFrameWork
+//
+//  Created by 온석태 on 11/4/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RecordManagerFrameWork.
+FOUNDATION_EXPORT double RecordManagerFrameWorkVersionNumber;
+
+//! Project version string for RecordManagerFrameWork.
+FOUNDATION_EXPORT const unsigned char RecordManagerFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RecordManagerFrameWork/PublicHeader.h>
+
+
---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "RecordManagerLibrary",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "RecordManagerLibrary",
+            targets: ["RecordManagerLibrary"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "RecordManagerFrameWork", // .xcframework 타겟 참조
+            path: "Frameworks/RecordManagerFrameWork.xcframework" // .xcframework 경로
+        ),
+        .binaryTarget(
+            name: "LogManager", // .xcframework 타겟 참조
+            path: "Frameworks/LogManager.xcframework" // .xcframework 경로
+        ),
+        .target(
+            name: "RecordManagerLibrary",
+            dependencies: [
+                .target(name: "RecordManagerFrameWork"),
+                .target(name: "LogManager")
+            ]
+        ),
+
+    ]
+)
